bcbd02f463f6eee27bcc26b6abcfbc32a8b199a2
unknown <AYassinee2007@gmail.com>
2022-03-27 17:50:02 -0400

    improved build.bat

--- build.bat
+++ build.bat
@@ -1,10 +1,16 @@
 @echo off
 for %%I in (.) do set CurrDirName=%%~nxI
 set /p mode=Debug or Release(d/r):
+echo compiling cpp files...
 g++ -I dependencies/include -c src/*.cpp
+echo compilation over!
+echo building executable file...
 g++ *.o -o %CurrDirName% -L dependencies/lib -l sfml-graphics -l sfml-window -l sfml-system -l sfml-audio
+echo executable file successfully created!
 del *.o
 
-if "%mode%"=="r" robocopy %cd% "bin" /XD "dependencies" "src" "bin" ".vscode" /S & del "bin\build.bat" & del "bin\Setup_SFML.bat" & del %CurrDirName%".exe" & del "bin\.gitignore" & del "bin\.gitattributes"
+if "%mode%"=="r" echo setting up release files... & robocopy %cd% "bin" /XD "dependencies" "src" "bin" ".vscode" /S NFL /NDL /NJH /NJS /nc /ns & del "bin\build.bat" & del %CurrDirName%".exe" &  echo release files successfully created(check the bin folder)!
 
+echo Build finished!
+if "%mode%"=="d" %CurrDirName%
 cmd /k
