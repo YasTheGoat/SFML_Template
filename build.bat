0fd6c758916cdea0b0bac44145b2f0a95dd38dc5
unknown <AYassinee2007@gmail.com>
2022-03-28 09:11:36 -0400

    yeet

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
+
+if "%1"=="" exit 1
 for %%I in (.) do set CurrDirName=%%~nxI
-set /p mode=Debug or Release(d/r):
 echo compiling cpp files...
 g++ -I dependencies/include -c src/*.cpp
 echo compilation over!
@@ -9,8 +10,8 @@ g++ *.o -o %CurrDirName% -L dependencies/lib -l sfml-graphics -l sfml-window -l 
 echo executable file successfully created!
 del *.o
 
-if "%mode%"=="r" echo setting up release files... & robocopy %cd% "bin" /XD "dependencies" "src" "bin" ".vscode" /S NFL /NDL /NJH /NJS /nc /ns & del "bin\build.bat" & del %CurrDirName%".exe" &  echo release files successfully created(check the bin folder)!
+if "%1"=="r" echo setting up release files... & robocopy %cd% "bin" /XD "dependencies" "src" "bin" ".vscode" /S NFL /NDL /NJH /NJS /nc /ns & del "bin\build.bat" & del %CurrDirName%".exe" &  echo release files successfully created(check the bin folder)!
 
 echo Build finished!
-if "%mode%"=="d" %CurrDirName%
+if "%1"=="d" %CurrDirName%
 cmd /k
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,39 @@
+// To quickly build or release your sfml project
+// create a tasks.json file in the .vscode folder and paste this:
+//      "version": "2.0.0",
+//      "tasks": [
+//          {
+//              "label": "debug",
+//              "type": "shell",
+//              "command": "build.bat d",
+//              "problemMatcher": [],
+//              "presentation": {
+//                  "echo": false,
+//                  "reveal": "always",
+//                  "focus": false,
+//                  "panel": "shared",
+//                  "showReuseMessage": true,
+//                  "clear": false,
+//              }
+//          },
+//          {
+//              "label": "release",
+//              "type": "shell",
+//              "command": "build.bat r",
+//              "problemMatcher": [],
+//              "presentation": {
+//                  "echo": false,
+//                  "reveal": "always",
+//                  "focus": false,
+//                  "panel": "shared",
+//                  "showReuseMessage": true,
+//                  "clear": false,
+//              }
+//          }
+//      ]
+//  }
+// Now you simply have to press F5  and chose between build and release.
+
 #include <SFML/Graphics.hpp>
 
 int main()
